5cca4b06339c75c11633d78827027b727bdd9af9
fengshiyuan0924 <95137537+fengshiyuan0924@users.noreply.github.com>
2025-02-01 07:12:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const config = {
   mode: "in", // 签到:in,签退:out
   accounts: [
     {
-      username: "", //用户名
-      password: "", //密码
-      openId: "", //微信小程序抓包openid(可选)
-      unionId: "", //微信小程序抓包unionId(可选)
+      username: "17362086561", //用户名
+      password: "Zhugeqinmo2", //密码
+      openId: "ooru94jLO0RRNomJguovjx9_rQik", //微信小程序抓包openid(可选)
+      unionId: "oHY-uwTrZDa7Axvk-uUxMUztbEPo", //微信小程序抓包unionId(可选)
       sign: true, //是否自动签到
       reSign: false, //是否重新签到
       location: "", //经纬度 (可选),例如 "120.210792,30.246026"、"经度,纬度",不填写则自动获取（推荐）
